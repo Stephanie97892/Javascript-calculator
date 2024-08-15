b1389749a37b370c29d5b43d4f54ce3244214fae
Stephanie97892 <stephaniedavis10@hotmail.com>
2024-08-15 22:17:39 +0100

    re-designed
    changed the sizes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // VARIABLES
 
+//these are blank because they  will be redefined .
 let operator='';
 let previousValue='';
 let currentValue='';
@@ -8,7 +9,6 @@ let currentValue='';
 //symbols 
 let clear=document.querySelector("#clear-btn");
 let equal=document.querySelector(".equalsbtn");
-
 
 //numbers and decimal 
 let decimal=document.querySelector(".decimalBtn");
@@ -21,105 +21,108 @@ let calcScreen=document.querySelector("#screen");
 
 
 //NUMBERS
-
-function getNumberContent(number){
-    if(currentValue.length <= 10){
-        currentValue +=number;
+// this function selects the number and creates it by concatenation 
+function getNumberContent(number){  // take a number as a paramenter, 
+    if(currentValue.length <= 10){ // if the current value is less than 10 , can only have a max of 10 number  
+        currentValue +=number; // add number to currentvalue to make a string of number on the sreen 
+        updateScreen(); 
     }   
 }
 
-allNumbers.forEach((number) => number.addEventListener("click", function(e){
-    getNumberContent(e.target.textContent);
-    calcScreen.textContent = currentValue;
-
+allNumbers.forEach((number) => number.addEventListener("click", function(e){  // for each element add a click eventlistener 
+    getNumberContent(e.target.textContent); //calling the function and  passing the textcontent as the argument.  this will add the cumber to the current value 
 }));
 
 
+function updateScreen(){     //! created a new function 
+        if (previousValue !== '' && operator !== ''){   // the if statement used depending on the calculation 
+            calcScreen.textContent =  `${previousValue} ${operator} ${currentValue}`;
+        } else {
+            calcScreen.textContent = currentValue;    // makes the text content of the calc screen the same as the current value .
+    }
+}
 
 
 //OPERATORS
-
-function getOperatorContent(op){
-   
-    previousOperator=operator;
-    operator = op;
-    if(previousValue != "" && currentValue !=""){
-        calculate();
+//! This function fixed the issuse of creating cotinuous sums 
+function getOperatorContent(op){    // function declared takes parameter - op 
+    if (currentValue !== '') {
+        if (previousValue !== '') {  // nesting the if statements evaluated them seprately 
+            calculate();
+        } else {
+            previousValue = currentValue;
+        }
+        currentValue = '';
+        operator = op;
+        updateScreen(); 
     }
-    previousValue=currentValue;
-    currentValue=''
-          
+     
 }
 
-operators.forEach((op) => op.addEventListener("click", function(e){
-    getOperatorContent(e.target.textContent);  
-     calcScreen.textContent= previousValue + " " + operator;
-      
+// when you select multiple elements using a class name, the variable holding these elements is seen as an object.
+// for each is a method on the variable it has (parameter)
+//arrow function after => is the code block 
+operators.forEach((op) => op.addEventListener("click", function(e){ // for each op run the code block , add event event listener on eah op, the function is on the click (e) 
+    getOperatorContent(e.target.textContent); // function block : calls function and gets operator value 
+       
 }));
 
 
 
 
 //CLEAR BUTTON
-clear.addEventListener("click", function(){
+clear.addEventListener("click", function(){  // add event listner on the clear buttnon , once run it will clear all valus. 
     previousValue='';
     currentValue='';
     operator='';
-   calcScreen.textContent=currentValue;
-
+   calcScreen.textContent=currentValue; // clears the screen 
 });
 
 
 //DECIMAL
-
-function addDecimal(){
-    if(!currentValue.includes(".")){
-        calcScreen.textContent= currentValue += ".";
+decimal.addEventListener("click",function(){ // actiavtes the function above only when the decimal is clicked.
+    if(!currentValue.includes(".")){  // if the current value doesnt included a decimal run the code block
+        calcScreen.textContent= currentValue += "."; // concatenates it to the current value on the screen.
         
     }
-}
-
-decimal.addEventListener("click",function(){
-    addDecimal();
 });
-
-
-
 
 
 //EQUAL
 
-equal.addEventListener("click",function(){
-    calculate()
-        calcScreen.textContent=previousValue;
-        currentValue=''
-
+equal.addEventListener("click",function(){ // add eventlistener to the = button , when clicked run calculate function 
+    if (previousValue !== '' && currentValue !== '') {
+        calculate(); // Perform the calculation
+        calcScreen.textContent = previousValue; // Display the result
+        currentValue = previousValue; // Use result for next calculation
+        previousValue = ''; // Clear previousValue to prepare for next operation
+        operator = '';}
+  
 })
 
-function calculate(){
-    previousValue=Number(previousValue);
-    currentValue=Number(currentValue);
+function calculate(){  // calculate function 
+    previousValue=parseFloat(previousValue); // convert values to numbers 
+    currentValue=parseFloat(currentValue); // '' 
     
-    if(operator==="+"){
-        previousValue += currentValue;
-        currentValue = previousValue;
+    if(operator==="+"){     // if the operator matches run code
+        previousValue += currentValue; // previous values + current value sum - short for previous value = previous value + current value
     }else if(operator === "-"){
-        previousValue -= currentValue;
-        currentValue = previousValue;
+        previousValue -= currentValue;   
 
     }else if(operator === "/"){
-        previousValue /= currentValue;
-        currentValue = previousValue;
+        previousValue /= currentValue;        
         
-    }else if(operator === "*"){
-        previousValue *= currentValue;
-        currentValue = previousValue;  
+    }else if(operator === "x"){
+        previousValue *= currentValue;         
     }
-    previousValue=previousValue.toString();
-    currentValue=currentValue.toString();
-    
+    previousValue=previousValue.toString(); // covert to string so it can be shows in the screen 
+    currentValue='';   
     
 }
+
+ 
+
+  
 
 
 
